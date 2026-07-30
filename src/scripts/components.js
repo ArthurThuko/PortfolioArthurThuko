0c12c435378f64806ea446161df2163de2d767ec
ArthurThuko <arthurhenriqsfc@gmail.com>
2026-07-30 08:32:10 -0300

    feat: adicionar seção de tecnologias ao portfólio com ícones e estilos

--- src/scripts/components.js
+++ src/scripts/components.js
@@ -8,6 +8,7 @@ async function init() {
   await loadComponent("navbar", "./components/navbar.html");
   await loadComponent("inicio", "./components/inicio.html");
   await loadComponent("sobre", "./components/sobre.html");
+  await loadComponent("tecnologias", "./components/tecnologias.html");
 
   initThemeToggle();
   initMapWrap();
